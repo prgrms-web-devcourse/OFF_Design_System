02f3cd5d59448d91a448fdd8d16401dc9c6d0f8a
Lim_JiSeon <dpf226@naver.com>
2023-10-06 01:14:56 +0900

    :art: 코드 리뷰 피드백 반영

--- src/components/uploads/ImageUploader.tsx
+++ src/components/uploads/ImageUploader.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 
-export const defaultImg =
+export const DEFAULT_IMAGE =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2Riw8iKZ4n4XAI3zEc0ShoGo1S5Z-oQdRWw&usqp=CAU";
 
 type ImageUploaderPropsType = {
@@ -11,23 +11,30 @@ type ImageUploaderPropsType = {
 };
 
 const ImageUploader = ({
-  buttonImg = defaultImg,
+  buttonImg = DEFAULT_IMAGE,
   onChange,
   shape = "rounded",
   size = 200,
 }: ImageUploaderPropsType) => {
   const [image, setImage] = useState(buttonImg);
-  const fileInput = useRef<HTMLInputElement | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const shapeConfig = {
+    rounded: "rounded-lg",
+    circle: "rounded-full",
+    square: "rounded-none",
+  };
 
   const uploadHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement;
     const file = target.files![0];
     if (file) {
       if (onChange) onChange(target.files);
+
       setImage(file.toString());
       previewImage(file);
     } else {
-      setImage(defaultImg);
+      setImage(image);
       previewImage(null);
     }
   };
@@ -50,19 +57,13 @@ const ImageUploader = ({
         accept="image/jpg, image/png, image/jpeg"
         name="img_uploader"
         onChange={uploadHandler}
-        ref={fileInput}
+        ref={fileInputRef}
       />
       <img
-        className={`${
-          shape === "rounded"
-            ? "rounded-lg"
-            : shape === "circle"
-            ? "rounded-full"
-            : ""
-        }`}
+        className={`${shapeConfig[shape]}`}
         style={{ width: `${size}px`, height: `${size}px` }}
         src={image}
-        onClick={() => fileInput.current?.click()}
+        onClick={() => fileInputRef.current?.click()}
       />
     </>
   );
