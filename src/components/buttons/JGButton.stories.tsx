be77413419b8f05eb17ca85bf68b0766d8ee85d9
jgjgill <dbdltm22@naver.com>
2023-10-05 22:14:59 +0900

    feat: 스토리북 배포 자동화 작업

---
+++ src/components/buttons/JGButton.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from "@storybook/react";
+import JGButton from "./JGButton";
+
+const meta: Meta<typeof JGButton> = {
+  title: "Mocks/JGButton",
+  component: JGButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof JGButton>;
+
+export const Example: Story = {
+  render: () => <JGButton>jgjgill</JGButton>,
+};
