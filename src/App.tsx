71375a72fa3cf83c2da5182bfba0596d8d0f5cd0
Lim_JiSeon <dpf226@naver.com>
2023-10-05 18:17:40 +0900

    :tada: 이미지 업로더 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import JGButton from "./components/buttons/JGButton";
+import ImageUploader from "./components/uploads/ImageUploader";
 
 function App() {
-  return <JGButton>jgjgill</JGButton>;
+  return (
+    <>
+      <JGButton>jgjgill</JGButton>
+      <ImageUploader></ImageUploader>
+    </>
+  );
 }
 
 export default App;
