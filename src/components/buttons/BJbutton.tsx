e1aa91756031a6663a590c94979bc614ef03ea09
euan-lee <maplestory1419@naver.com>
2023-10-06 15:54:33 +0900

    refactor:button component리펙토링

--- src/components/buttons/BJbutton.tsx
+++ src/components/buttons/BJbutton.tsx
@@ -1,31 +1,21 @@
 import React from "react";
 
 type BJButton = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  size: 'sm' | 'md' | 'lg';
-  btnName:string;
+  size?: 'sm' | 'md' | 'lg';
 };
 
-const BJButton = ({size,btnName,...props}: BJButton ) => {
+const BJButton = ({size='sm',...props}: BJButton ) => {
   const { className, ...restProps } = props;
   const baseStyle = `${className} bg-green-400 hover:bg-blue-700`;
 
-  let sizeStyle = 'sm';
-  switch(size) {
-    case 'sm':
-      sizeStyle = 'py-1 px-2 text-sm';
-      break;
-    case 'md':
-      sizeStyle = 'py-2 px-4 text-base';
-      break;
-    case 'lg':
-      sizeStyle = 'py-3 px-6 text-lg';
-      break;
-    default:
-      break;
+    const sizeStyle = {
+    'sm': 'py-1 px-2 text-sm',
+    'md':'py-2 px-4 text-base',
+    'lg':'py-3 px-6 text-lg',
   }
+ 
   return (
-    <button {...restProps} className={`${baseStyle} ${sizeStyle}`}>
-        {btnName}
+    <button {...restProps} className={`${baseStyle} ${sizeStyle[size]}`}>
     </button>
   );
 }
