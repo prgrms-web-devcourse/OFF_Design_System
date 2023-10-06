8da454ee8ca7068a71d2c711f02dfe5702887106
euan-lee <maplestory1419@naver.com>
2023-10-06 23:58:23 +0900

    feat:bj button 스토리북 배포

---
+++ src/components/buttons/BJButton.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from "@storybook/react";
+import BJButton from "./BJButton";
+
+
+const meta: Meta = {
+  title: "BUTTON/bySize/BJButton",
+  component: BJButton,
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'] ,
+      control: { type: 'select' },
+    },
+  },
+
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BJButton>;
+
+export const Example: Story = {
+  render: (args) => <BJButton  {...args}>범진 버튼 test</BJButton>,
+};
