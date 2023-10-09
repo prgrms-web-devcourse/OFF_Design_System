927e97e660c2e73f646ea599e9b2056dab40f6db
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-10 04:40:19 +0900

    feat: 라이브러리 배포를 위한 Input컴포넌트 추가

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export { default as JGButton } from "./src/components/buttons/JGButton";
-export { default as ImageUploader } from "./src/components/uploads/ImageUploader/ImageUploader";
+export { default as JGButton } from './src/components/buttons/JGButton';
+export { default as ImageUploader } from './src/components/uploads/ImageUploader/ImageUploader';
+export { default as Input } from './src/components/input/Input';
