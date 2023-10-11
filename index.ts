1f4f182c322a0d7138a787300d71eede99e9376a
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-11 20:43:24 +0900

    fix: index.ts 컨플릭 test

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 export { default as JGButton } from './src/components/buttons/JGButton';
 export { default as Textarea } from './src/components/textarea/Textarea';
 export { default as ImageUploader } from './src/components/uploads/ImageUploader/ImageUploader';
-export { default as Input } from './src/components/input/Input';
