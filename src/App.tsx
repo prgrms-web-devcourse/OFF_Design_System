51181c79ec66352278c6bc7cdbeefcbed1badc54
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-11 18:51:03 +0900

    fix: app.tsx 컴플릭 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import JGButton from './components/buttons/JGButton';
-import Input from './components/input/Input';
 import ImageUploader from './components/uploads/ImageUploader/ImageUploader';
 
 function App() {
   return (
     <>
       <JGButton>jgjgill</JGButton>
-      <ImageUploader></ImageUploader>
-      <Input
-        inputLabel='ID'
-        type='text'
-        placeholder='아이디'
-        style={{ color: 'blue' }}
-      />
-      <Input inputLabel='PASSWORD' type='password' placeholder='비밀번호' />
+      <ImageUploader />
     </>
   );
 }
