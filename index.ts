e05a61ace7bfa43812204698717b75cb696a5f0b
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-16 00:01:08 +0900

    feat: index.ts파일에 input컴포넌트 추가 (#45)

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export { default as Textarea } from "./src/components/textarea/Textarea";
 export { default as ImageUploader } from "./src/components/uploads/ImageUploader/ImageUploader";
 export { default as Avatar } from "./src/components/common/Avatar/Avatar";
 export { default as Badge } from "./src/components/badge/Badge";
+export { default as Input } from "./src/components/input/Input";
