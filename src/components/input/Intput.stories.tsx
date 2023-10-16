d551be630f85ed40474c1537040864c9bff470df
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-17 00:10:36 +0900

    feat: 스토리북 폴더구조 수정, 아이템 수정

--- src/components/input/Intput.stories.tsx
+++ src/components/input/Intput.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import Input from './Input';
 
 const meta: Meta<typeof Input> = {
-  title: 'Input',
+  title: 'components/Input',
   component: Input,
 };
 
@@ -11,13 +11,59 @@ type Story = StoryObj<typeof Input>;
 
 export const Default: Story = {
   args: {
+    inputLabel: '',
+    inputType: 'text',
+    placeholder: '',
+  },
+};
+
+Default.argTypes = {
+  inputLabel: {
+    control: { type: 'text' },
+  },
+  inputType: {
+    control: {
+      type: 'radio',
+    },
+    options: ['text', 'password'],
+  },
+  placeholder: {
+    control: { type: 'text' },
+  },
+};
+
+export const Id: Story = {
+  args: {
     inputLabel: 'ID',
     inputType: 'text',
     placeholder: '아이디',
   },
 };
 
-Default.argTypes = {
+Id.argTypes = {
+  inputLabel: {
+    control: { type: 'text' },
+  },
+  inputType: {
+    control: {
+      type: 'radio',
+    },
+    options: ['text', 'password'],
+  },
+  placeholder: {
+    control: { type: 'text' },
+  },
+};
+
+export const Password: Story = {
+  args: {
+    inputLabel: 'PASSWORD',
+    inputType: 'password',
+    placeholder: '비밀번호',
+  },
+};
+
+Password.argTypes = {
   inputLabel: {
     control: { type: 'text' },
   },
